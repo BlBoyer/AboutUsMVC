1bec5a4e753541255e98ca7b3e99c12aaf2ca932
BLBoyer <boyer.benlee@gmail.com>
2022-04-10 14:18:52 -0700

    put swagger into production momentarily

--- Program.cs
+++ Program.cs
@@ -25,10 +25,9 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
-} else {
-    app.UseSwagger();
-    app.UseSwaggerUI();
 }
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseHttpsRedirection();
 app.UseStaticFiles();
 app.UseRouting();
